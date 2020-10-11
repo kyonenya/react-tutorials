064683707c7bd8fa5307a57ba680fde1515e4721
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-10-11 09:25:51 +0900

    関数コンポーネントまで修了（1〜9）

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render(React.createElement("h2", null, "\u3053\u3093\u306B\u3061\u306F\u3001\u4E16\u754C"), document.getElementById('root'));
+// functionで定義する
+function Counter(props) {
+    // クリックイベント
+    const clicked = (color) => {
+        alert(color);
+    };
+    return React.createElement("li", { style: { backgroundColor: props.color }, 
+        // 9. イベントに引数を渡したい場合はonClickに無名関数を渡す
+        onClick: () => clicked(props.color) }, "0");
+}
+ReactDOM.render(React.createElement("div", { className: "container" },
+    React.createElement("ul", null,
+        React.createElement(Counter, { color: "tomato" }),
+        React.createElement(Counter, { color: "skyblue" }),
+        React.createElement(Counter, { color: "limegreen" }))), document.getElementById('root'));
+// 4-5. JSXの基本
+const name = '隆史';
+const showMessage = () => {
+    alert('こんにちは');
+};
+ReactDOM.render(React.createElement("div", { className: "box", 
+    // style属性をオブジェクトで指定する
+    // JavaScriptの式なので合わせて二重波括弧になる（クォーテーションは無し）
+    style: { width: 100, height: 100, backgroundColor: 'tomato' }, 
+    // onClick属性はクォーテーション無しの波括弧で指定する
+    onClick: showMessage },
+    "// JavaScript\u306E\u5F0F\u306F\u6CE2\u62EC\u5F27\u3067\u57CB\u3081\u8FBC\u3080",
+    name), document.getElementById('foo'));
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render(<h2>こんにちは、世界</h2>, document.getElementById('root'));
+
+
+// 6. 関数コンポーネント
+// propsにはタイプエイリアスで型を付ける
+type CounterProps = {
+  color: string
+}
+
+// functionで定義する
+function Counter(props: CounterProps) {  // 7. 渡ってきたpropsを吸い上げる
+  // クリックイベント
+  const clicked = (color: string) => {
+    alert(color);
+  }
+
+  return <li 
+    style={{backgroundColor: props.color}}
+    // 9. イベントに引数を渡したい場合はonClickに無名関数を渡す
+    onClick={() => clicked(props.color)}
+  >
+    0
+  </li>;
+}
+
+ReactDOM.render(
+  <div className="container">
+    <ul>
+      {/* 6. props: 同種のコンポーネントを識別する属性'color'を自分で定義できる */}
+      <Counter color="tomato" />
+      <Counter color="skyblue" />
+      <Counter color="limegreen" />
+    </ul>
+  </div>,
+  document.getElementById('root')
+);
+
+
+// 4-5. JSXの基本
+const name = '隆史';
+const showMessage = () => {
+  alert('こんにちは');
+}
+ReactDOM.render( 
+  <div 
+    className="box" 
+    // style属性をオブジェクトで指定する
+    // JavaScriptの式なので合わせて二重波括弧になる（クォーテーションは無し）
+    style={{width: 100, height: 100, backgroundColor: 'tomato'}} 
+    // onClick属性はクォーテーション無しの波括弧で指定する
+    onClick={showMessage}
+  >
+    // JavaScriptの式は波括弧で埋め込む
+    {name}
+  </div>,
+  document.getElementById('foo')
+);
