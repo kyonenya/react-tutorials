43159ad7459e5a9aa6584048309b5e0176923e56
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-10-12 02:08:57 +0900

    修了。state更新スイッチを子まで渡し、子から引数を親に渡しつつスイッチを押す。

--- dist/app.js
+++ dist/app.js
@@ -1,43 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// 子コンポーネントはstateを持つ必要がないので関数コンポーネントにする
 /**
  *
  * @params counter
  */
 const Counter = (props) => {
     console.log(props.counter);
-    return React.createElement("li", { style: { backgroundColor: props.counter.color } },
+    return React.createElement("li", { style: { backgroundColor: props.counter.color }, 
+        // 17. 親要素からstate更新スイッチを受け取ってイベントに紐付ける
+        onClick: () => props.countUp(props.counter) },
         props.counter.id,
         "\uFF1A",
         props.counter.count);
 };
-// 16. コンポーネントをループで一括生成する
+// 子コンポーネントはstateを持つ必要がないので関数コンポーネントにした
 /**
- *
+ * コンポーネントをループで一括生成する（16.）
  * @params counters
+ * @render Counter(counter)
  */
 const CounterList = (props) => {
     const counters = props.counters.map((counter) => {
         return React.createElement(Counter, { counter: counter, 
             // ユニークなkeyが必要、ループで処理するコンポーネントには
-            key: counter.id });
+            key: counter.id, countUp: props.countUp });
     });
     return React.createElement("ul", null, counters);
 };
-// 14. 共通要素でstateを管理する
 /**
  * App
- *
+ * 共通要素でstateを管理する（14.）
+ * @render CounterList(counters)
  */
 class App extends React.Component {
     constructor(props) {
         super(props); // 実際にはpropsを受け取らなくても再定義してやる必要がある
+        // 18. thisで渡ってきたカウンターのidと同じカウンターのcountを1増やす
+        this.countUp = (counter) => {
+            this.setState((prevState) => {
+                // prevState.count++ とするわけにはいかないので、値のコピーをとる（イミュータブルに）
+                const counters = prevState.counters.map((counter) => {
+                    return { id: counter.id, count: counter.count, color: counter.color };
+                });
+                const pos = prevState.counters.map((counter) => {
+                    return counter.id; // [A, B, C]
+                }).indexOf(counter.id); // Cなら'2'
+                counters[pos].count++;
+                return {
+                    counters: counters,
+                    total: prevState.total + 1,
+                };
+            });
+        };
         this.render = () => {
             return React.createElement("div", { className: "container" },
-                "// \u5B50\u30B3\u30F3\u30DD\u30FC\u30CD\u30F3\u30C8\u306B\u5404state\u3092\u4E38\u3054\u3068\u6E21\u3059",
-                React.createElement(CounterList, { counters: this.state.counters }),
-                React.createElement("div", null, "\u5408\u8A08: 36"));
+                React.createElement(CounterList
+                // 子コンポーネントのpropsにstateを丸ごと渡す
+                , { 
+                    // 子コンポーネントのpropsにstateを丸ごと渡す
+                    counters: this.state.counters, 
+                    // 17. state更新スイッチを末端の子コンポーネントまで渡していく
+                    countUp: this.countUp }),
+                React.createElement("div", null,
+                    "\u5408\u8A08: ",
+                    this.state.total));
         };
         this.state = {
             // 15. 各カウンターのstateをここから分配していく
@@ -45,7 +71,8 @@ class App extends React.Component {
                 { id: 'A', count: 0, color: 'tomato' },
                 { id: 'B', count: 0, color: 'skyblue' },
                 { id: 'C', count: 0, color: 'limegreen' },
-            ]
+            ],
+            total: 0,
         };
     }
 }
--- src/app.tsx
+++ src/app.tsx
@@ -11,29 +11,30 @@ type Counter = {
  * 
  * @params counter
  */
-const Counter = (props: {counter: Counter}) => {
+const Counter = (props: {counter: Counter, countUp: (counter :Counter) => void}) => {
   console.log(props.counter);
   return  <li
     style={{backgroundColor: props.counter.color}}
+    // 17. 親要素からstate更新スイッチを受け取ってイベントに紐付ける
+    onClick={() => props.countUp(props.counter)}
   >
     {props.counter.id}：{props.counter.count}
   </li>; 
 }
 // 子コンポーネントはstateを持つ必要がないので関数コンポーネントにした
 
-
-// 16. コンポーネントをループで一括生成する
 /**
- * 
+ * コンポーネントをループで一括生成する（16.）
  * @params counters
  * @render Counter(counter)
  */
-const CounterList = (props: {counters: Counter[]}) => {
+const CounterList = (props: {counters: Counter[], countUp: (counter: Counter) => void}) => {
   const counters = props.counters.map((counter: Counter) => {
     return <Counter 
       counter={counter}
       // ユニークなkeyが必要、ループで処理するコンポーネントには
       key={counter.id}
+      countUp={props.countUp}
     />
   });
   return <ul>
@@ -42,12 +43,12 @@ const CounterList = (props: {counters: Counter[]}) => {
 }
 
 
-// 14. 共通要素でstateを管理する
 /**
  * App
+ * 共通要素でstateを管理する（14.）
  * @render CounterList(counters)
  */
-class App extends React.Component<{}, {counters: Counter[]}> {
+class App extends React.Component<{}, {counters: Counter[], total: number}> {
   constructor(props: {}) {
     super(props);  // 実際にはpropsを受け取らなくても再定義してやる必要がある
     this.state = {
@@ -56,14 +57,39 @@ class App extends React.Component<{}, {counters: Counter[]}> {
         {id: 'A', count: 0, color: 'tomato'},
         {id: 'B', count: 0, color: 'skyblue'},
         {id: 'C', count: 0, color: 'limegreen'},
-      ]
+      ], 
+      total: 0,
     }
   }
+  
+  // 18. thisで渡ってきたカウンターのidと同じカウンターのcountを1増やす
+  countUp = (counter: Counter) => {  // どのカウンターコンポーネントを更新するのか、引数をとる
+    this.setState((prevState) => {
+      // prevState.count++ とするわけにはいかないので、値のコピーをとる（イミュータブルに）
+      const counters = prevState.counters.map((counter) => {
+        return {id: counter.id, count: counter.count, color: counter.color};
+      });
+      const pos = prevState.counters.map((counter) => {
+        return counter.id;  // [A, B, C]
+      }).indexOf(counter.id);  // Cなら'2'
+      counters[pos].count++;
+      
+      return {
+        counters: counters,
+        total: prevState.total + 1,
+      };
+    });
+  }
+  
   render = () => {
     return <div className="container">
-      {/* 子コンポーネントに各stateを丸ごと渡す */}
-      <CounterList counters={this.state.counters}/>
-      <div>合計: 36</div>
+      <CounterList 
+        // 子コンポーネントのpropsにstateを丸ごと渡す
+        counters={this.state.counters}
+        // 17. state更新スイッチを末端の子コンポーネントまで渡していく
+        countUp={this.countUp}
+      />
+      <div>合計: {this.state.total}</div>
     </div>
   }
 }
