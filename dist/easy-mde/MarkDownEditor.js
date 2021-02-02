882c76937b740bed8adf00e166cd22183d6d8ab9
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-02-02 12:05:00 +0900

    easyMDE エラー修正

--- dist/easy-mde/MarkDownEditor.js
+++ dist/easy-mde/MarkDownEditor.js
@@ -3,12 +3,13 @@ import SimpleMDE from 'react-simplemde-editor';
 //import 'easymde/dist/easymde.min.css';
 export const MarkDownEditor = () => {
     const [text, setText] = useState('');
+    const [instance, setInstance] = useState(null);
     return (React.createElement(SimpleMDE
     // @ts-ignore EasyMDEの型定義が壊れてる。togglePreview()はstaticメソッドなのだが、prototype拡張をしてインスタンスのメソッドに見せているため
     , { 
         // @ts-ignore EasyMDEの型定義が壊れてる。togglePreview()はstaticメソッドなのだが、prototype拡張をしてインスタンスのメソッドに見せているため
-        getMdeInstance: (instance) => instance.togglePreview(), onChange: (txt) => {
+        getMdeInstance: (i) => setInstance(i), onChange: (txt) => {
             setText(txt);
-            console.log(txt);
+            console.log(instance);
         } }));
 };
--- src/easy-mde/MarkDownEditor.tsx
+++ src/easy-mde/MarkDownEditor.tsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SimpleMDE from 'react-simplemde-editor';
 //import 'easymde/dist/easymde.min.css';
 
 export const MarkDownEditor = () => {
   const [text, setText] = useState('');
-  
+  const [instance, setInstance] = useState(null);
+
   return (
     <SimpleMDE
       // @ts-ignore EasyMDEの型定義が壊れてる。togglePreview()はstaticメソッドなのだが、prototype拡張をしてインスタンスのメソッドに見せているため
-      getMdeInstance={(instance) => instance.togglePreview()}
+      getMdeInstance={(i) => setInstance(i)}
       onChange={(txt) => {
         setText(txt);
-        console.log(txt);
+        console.log(instance);
       }}
     />
   );
